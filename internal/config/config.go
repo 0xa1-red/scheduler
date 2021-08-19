0be30d8551b60d135fcc3b70b153f5c970732c04
Alfred Dobradi <alfred.dobradi@digitgaming.com>
2021-08-19 20:11:36 +0100

    feat: config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,27 @@
 // Package config contains the global configuration
 // and the means to set and retrieve values
 package config
+
+import (
+	"os"
+
+	"hq.0xa1.red/axdx/config"
+)
+
+type Config struct {
+	Test string
+}
+
+func Load(path string) (*config.Configuration, error) {
+	fp, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := config.Load(&Config{}, fp)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
